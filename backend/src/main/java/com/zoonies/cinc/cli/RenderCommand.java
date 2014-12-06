819428135e0baf2a58ac832fe7db2724810f1119
Patrick Auld <pauld@birst.com>
2014-12-06 00:29:34 -0800

    Initial import of backend service

---
+++ backend/src/main/java/com/zoonies/cinc/cli/RenderCommand.java
@@ -0,0 +1,50 @@
+package com.zoonies.cinc.cli;
+
+import com.google.common.base.Optional;
+import com.zoonies.cinc.HelloWorldConfiguration;
+import com.zoonies.cinc.core.Template;
+
+import io.dropwizard.cli.ConfiguredCommand;
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RenderCommand extends ConfiguredCommand<HelloWorldConfiguration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RenderCommand.class);
+
+    public RenderCommand() {
+        super("render", "Render the template data to console");
+    }
+
+    @Override
+    public void configure(Subparser subparser) {
+        super.configure(subparser);
+        subparser.addArgument("-i", "--include-default")
+                 .action(Arguments.storeTrue())
+                 .dest("include-default")
+                 .help("Also render the template with the default name");
+        subparser.addArgument("names").nargs("*");
+    }
+
+    @Override
+    protected void run(Bootstrap<HelloWorldConfiguration> bootstrap,
+                       Namespace namespace,
+                       HelloWorldConfiguration configuration) throws Exception {
+        final Template template = configuration.buildTemplate();
+
+        if (namespace.getBoolean("include-default")) {
+            LOGGER.info("DEFAULT => {}", template.render(Optional.<String>absent()));
+        }
+
+        for (String name : namespace.<String>getList("names")) {
+            for (int i = 0; i < 1000; i++) {
+                LOGGER.info("{} => {}", name, template.render(Optional.of(name)));
+                Thread.sleep(1000);
+            }
+        }
+    }
+}

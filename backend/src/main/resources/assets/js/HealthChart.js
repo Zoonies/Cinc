d725fa2a25e3dd40236008ce9afdeb75498068ab
koding user <shaarptooth@gmail.com>
2014-12-07 07:03:18 +0000

    Front-End Chart Updates
    Dataset loading updates
    Style Updates

--- backend/src/main/resources/assets/js/HealthChart.js
+++ backend/src/main/resources/assets/js/HealthChart.js
@@ -1,115 +1,118 @@
 //Load Config
-$.getScript( "js/ChartGlobalConfig.js");
-$.getScript( "js/helpers.js");
-    //Test Data Set
-    var happinessRawData = {
-        "11/14/2014" : 1,
-        "11/15/2014" : 4,
-        "11/16/2014" : 3,
-        "11/17/2014" : 1,
-        "11/18/2014" : 1,
-        "11/19/2014" : 5,
-        "11/20/2014" : 4,
-        "11/21/2014" : 1,
-        "11/22/2014" : 4,
-        "11/23/2014" : 3,
-        "11/24/2014" : 1,
-        "11/25/2014" : 1,
-        "11/26/2014" : 5,
-        "11/27/2014" : 4,
-        "11/28/2014" : 1,
-        "11/29/2014" : 2,
-        "11/30/2014" : 4,
-        "12/1/2014" : 2,
-        "12/2/2014" : 5,
-        "12/3/2014" : "",
-        "12/4/2014" : 1,
-        "12/5/2014" : 2,
-        "12/5/2014" : 4
-    };
-    var pollenRawData = {
-        "11/14/2014" : 41,
-        "11/15/2014" : 54,
-        "11/16/2014" : 43,
-        "11/17/2014" : 21,
-        "11/18/2014" : 31,
-        "11/19/2014" : 25,
-        "11/20/2014" : 14,
-        "11/21/2014" : 34,
-        "11/22/2014" : 24,
-        "11/23/2014" : 73,
-        "11/24/2014" : 41,
-        "11/25/2014" : 41,
-        "11/26/2014" : 35,
-        "11/27/2014" : 34,
-        "11/28/2014" : 51,
-        "11/29/2014" : 42,
-        "11/30/2014" : 44,
-        "12/1/2014" : 32,
-        "12/2/2014" : 25,
-        "12/3/2014" : 55,
-        "12/4/2014" : 21,
-        "12/5/2014" : 43,
-        "12/5/2014" : 25
-    };
+$.getScript( "./js/ChartGlobalConfig.js");
 
 var healthChartConfig = {
     "days" : 7,
     "datasets" : ["happiness", "pollen"]
+}
+var healthData = {
+    //"happiness": [2,3,5,3.etc],
+    //"pollen": [34.23,43,etc]
+}
+var linesStyles = {
+    "happiness": {
+        label: "happiness",
+        fillColor: "rgba(52,52,119,0.2)",
+        strokeColor: "rgba(52,52,119,1)",
+        pointColor: "rgba(128, 128, 179,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)"
+    },
+    "pollen": {
+        label: "pollen",
+        fillColor: "rgba(170,121,57,0.2)",
+        strokeColor: "rgba(170,121,57,1)",
+        pointColor: "rgba(212, 167, 106,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)"
+    },
+    "crime": {
+        label: "pollen",
+        fillColor: "rgba(170,160,57,0.2)",
+        strokeColor: "rgba(170,160,57,1)",
+        pointColor: "rgba(212, 203, 106,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)"
+    },
+    "default": {
+        label: "happiness",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)"
+    }
+}
+
+//Gets the line style based on the set name
+var getStyle = function(setName)
+{
+    var style = linesStyles[setName];
+    if( typeof(style) == "undefined")
+         style = linesStyles["default"];
+    
+    return style;
+}
+
+//Get the datapoint in "setName" 
+//If no value is present an empty string is sent so ChartJS doesn't error out
+var getDataPoint = function(setName, date)
+{
+    var set = healthData[setName];
+    if(typeof(set) == "undefined")
+        return "";
+    var point = set[date];
+    if(typeof(point) == "undefined")
+        return "";
+    else
+        return point;
 }
 
 
 
 var getChartData = function()
 {
-
-    //Settings
+    //Number of days to display
     var numberOfHistoricalDays = healthChartConfig.days;
     
-    //Prep datasets
-    var happinessData=[];
-    var pollenData=[];
     //Date
     var today = new Date();
     var labels = [ ];
-    for (var i=numberOfHistoricalDays; i >= 1 ; i-- )
+    
+    //Final that is passed to CharJS
+    var data = {};
+    data.datasets = [];
+    
+    //Loop through for each dataset and pull out subset based on the given range.
+    for(var a = 0;  a < healthChartConfig.datasets.length; a++)
     {
-        var current = new Date();
-        current.setDate(today.getDate()-i);
-        var dateString =  dateToString(current);
-        labels.push(dateString);
-        var happyDayValue =  happinessRawData[dateString];
-        var pollenDayValue =  pollenRawData[dateString];
-        happinessData.push(happyDayValue);
-        pollenData.push(pollenDayValue+5);
+        var cData = []; //Subset
+        var setName = healthChartConfig.datasets[a]; //Name of set
+        for (var i=numberOfHistoricalDays; i >= 1 ; i-- )
+        {
+            var current = new Date();
+            current.setDate(today.getDate()-i);
+            var dateString =  dateToString(current); //Create the data string
+            
+            
+            var dataPoint = getDataPoint(setName, dateString); //Get the data point
+            cData.push(dataPoint); //Add to subset
+            
+            if(a === 0) //Only capture on first loop to prevent duplicates
+                labels.push(dateString); //Add to the X-Axis
+        }
+        
+        data.datasets.push({});
+        data.datasets[a].data = cData; //Add the data subset to the ChartJS object
+        $.extend(data.datasets[a], getStyle(setName)); //Add the line style 
+        
     }
-
-    //Example dataset
-    var data = {
-        labels: labels,
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: happinessData
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: pollenData
-            }
-        ]
-    };
+    
+    data.labels = labels; //Set the X-Axis
     return data;
 }
 
@@ -137,6 +140,7 @@ var populateInitialChart = function()
 $(document).ready(function () {
     //
     // PULL DATA FROM SERVER
+    pullDataSets();
     //
     
     // Get context with jQuery - using jQuery's .get() method.
@@ -150,8 +154,94 @@ $(document).ready(function () {
     healthChartConfig.canvas = canvas;
     
     //
-    // ON DATA PULL COMPLETE
-    //
+    // CALL ON DATA PULL COMPLETE (FOR ALL SETS)
     populateInitialChart();
-
+    //
 });
+
+var pullDataSets = function()
+{
+    //Pull the full range of data from the server.
+    //This is can be filtered by the user on the client side.
+    
+   //Test-Fake-Sample Data
+    var happinessRawData = {
+        "11/14/2014" : 1,
+        "11/15/2014" : 4,
+        "11/16/2014" : 3,
+        "11/17/2014" : 1,
+        "11/18/2014" : 1,
+        "11/19/2014" : 5,
+        "11/20/2014" : 4,
+        "11/21/2014" : 1,
+        "11/22/2014" : 4,
+        "11/23/2014" : 3,
+        "11/24/2014" : 1,
+        "11/25/2014" : 1,
+        "11/26/2014" : 5,
+        "11/27/2014" : 4,
+        "11/28/2014" : 1,
+        "11/29/2014" : 2,
+        "11/30/2014" : 4,
+        "12/1/2014" : 2,
+        "12/2/2014" : 5,
+        "12/3/2014" : "",
+        "12/4/2014" : 1,
+        "12/5/2014" : 2,
+        "12/6/2014" : 4
+    };
+    var pollenRawData = {
+        "11/14/2014" : 41,
+        "11/15/2014" : 54,
+        "11/16/2014" : 43,
+        "11/17/2014" : 21,
+        "11/18/2014" : 31,
+        "11/19/2014" : 25,
+        "11/20/2014" : 14,
+        "11/21/2014" : 34,
+        "11/22/2014" : 24,
+        "11/23/2014" : 73,
+        "11/24/2014" : 41,
+        "11/25/2014" : 41,
+        "11/26/2014" : 35,
+        "11/27/2014" : 34,
+        "11/28/2014" : 51,
+        "11/29/2014" : 42,
+        "11/30/2014" : 44,
+        "12/1/2014" : 32,
+        "12/2/2014" : 25,
+        "12/3/2014" : 55,
+        "12/4/2014" : 21,
+        "12/5/2014" : 43,
+        "12/6/2014" : 25
+    };
+    var crimeRawData = {
+        "11/14/2014" : 45,
+        "11/15/2014" : 23,
+        "11/16/2014" : 64,
+        "11/17/2014" : 45,
+        "11/18/2014" : 34,
+        "11/19/2014" : 23,
+        "11/20/2014" : 23,
+        "11/21/2014" : 54,
+        "11/22/2014" : 45,
+        "11/23/2014" : 45,
+        "11/24/2014" : 63,
+        "11/25/2014" : 35,
+        "11/26/2014" : 34,
+        "11/27/2014" : 22,
+        "11/28/2014" : 34,
+        "11/29/2014" : 42,
+        "11/30/2014" : 23,
+        "12/1/2014" : 23,
+        "12/2/2014" : 12,
+        "12/3/2014" : 43,
+        "12/4/2014" : 53,
+        "12/5/2014" : 11,
+        "12/6/2014" : 11
+    };
+    
+    healthData.happiness = happinessRawData;
+    healthData.pollen =  pollenRawData;
+    healthData.crime =  crimeRawData;
+}
--- backend/src/main/resources/assets/js/listeners.js
+++ backend/src/main/resources/assets/js/listeners.js
@@ -6,35 +6,52 @@ var clearActive = function(className, newActive)
     $(newActive).addClass("active").removeClass("btn-default"); 
 }
 
+//Toggles Active highlight
 var toggleActive = function(element)
 {
     $(element).toggleClass("active").toggleClass("btn-default"); 
 }
 
+//On click for the range selection
 $(".range").click(function (e) {
-    var days = $(this).data("range");
-    if(healthChartConfig.days != days)
+    var days = $(this).data("range"); //Days
+    if(healthChartConfig.days != days) //Don't update if there is no change
     {
-        clearActive("range-group", this);
+        clearActive("range-group", this); //Update UI
         healthChartConfig.days = days;
-        updateChart();
+        updateChart(); //Update chart
     }
 });
 
+//Set initiially active elements
+$(".dataset").each(function (index) {
+    var name = $(this).data("set-name"); //Get the name
+    if( $.inArray(name, healthChartConfig.datasets) > -1) //Check if it's in the array
+    {
+        toggleActive (this); // toggle to active
+        var color = linesStyles[name].fillColor;
+        $(this).css("background-color", color);
+    }
+});
+
+//On click when user changes the datasets they are viewing.
 $(".dataset").click(function (e) {
-     var name = $(this).data("set-name");
+     var name = $(this).data("set-name"); //Get the name
      
      if( $.inArray(name, healthChartConfig.datasets) > -1)
      {
          //Disable
          healthChartConfig.datasets.remove(name);
          updateChart();
+          $(this).css("background-color", "");
      }
      else
      {
          //Enable
          healthChartConfig.datasets.push(name);
-         updateChart;
+         updateChart();
+         var color = linesStyles[name].fillColor;
+         $(this).css("background-color", color);
      }
      toggleActive (this);
 });
